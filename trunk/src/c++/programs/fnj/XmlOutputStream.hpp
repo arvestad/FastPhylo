c5bb1bd7332b760bda93160274a2aa81aac6783b
eriksjolund <eriksjolund@ba7053ae-9dfc-443b-99ee-b7775b33cf57>
2008-08-14 09:30:30 +0000

    recreating the newick string from the newick-xml string

--- trunk/src/c++/programs/fnj/XmlOutputStream.hpp
+++ trunk/src/c++/programs/fnj/XmlOutputStream.hpp
@@ -4,6 +4,8 @@
 #include <cstdio>
 #include "DataOutputStream.hpp"
 
+
+
 class XmlOutputStream : public DataOutputStream
 {
 public:
@@ -12,6 +14,12 @@ public:
   virtual ~XmlOutputStream();
 protected:
   virtual void print( tree2int_map & tree2count, bool noCounts, std::vector<std::string> & names, Extrainfos & extrainfos );
+
+ /** printNewick unparses the xml string created before. Of course it would better if the SequenceTree would produce both a newick string
+     and some xml representation of newick. 
+**/
+  virtual void printNewick(std::ostream * fp , std::string s );
+  virtual void printNewickNode(std::ostream * fp , xmlNode * node);
 };
 
 #endif // XMLOUTPUTSTREAM_HPP
