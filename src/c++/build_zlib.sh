f668495e6624e8e899b347878c355e05f211e844
Erik Sjölund <erik.sjolund@gmail.com>
2008-10-08 13:40:18 +0000

    CPACK_GENERATOR=TGZ for MACOSX seems to work now

--- src/c++/build_zlib.sh
+++ src/c++/build_zlib.sh
@@ -4,4 +4,4 @@ zlibversion=$1
 installDir="$2"
 tar xfz "../download/zlib-${zlibversion}.tar.gz"
 cd zlib-${zlibversion}
-./configure  "--prefix=$installDir" && make install
+./configure  "--prefix=$installDir" && make && make install
