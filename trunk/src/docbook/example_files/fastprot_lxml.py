ee9ed908c194f5dcdf337ca6d90f82ad58fe0c9c
malagori <malagori@ba7053ae-9dfc-443b-99ee-b7775b33cf57>
2012-08-17 09:32:35 +0000

    fastphylo is stable now

---
+++ trunk/src/docbook/example_files/fastprot_lxml.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import sys
+from lxml import etree
+from copy import deepcopy
+
+for action, element in etree.iterparse(sys.stdin, tag="dm"):
+  dm_copy=deepcopy(element)
+  print dm_copy.xpath('count(row/entry[ number(.) < 0.1 ])')
