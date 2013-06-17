66cd93f06156a58b3df0869861bc24cd53043c8e
malagori <malagori@users.noreply.github.com>
2013-06-17 15:38:03 +0000

    removing redundent file

--- src/docbook/example_files/fastprot_lxml.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-import sys
-from lxml import etree
-from copy import deepcopy
-
-for action, element in etree.iterparse(sys.stdin, tag="dm"):
-  dm_copy=deepcopy(element)
-  print dm_copy.xpath('count(row/entry[ number(.) < 0.1 ])')
